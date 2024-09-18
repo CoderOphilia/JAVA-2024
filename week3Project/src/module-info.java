cf12abf35d0fd488548c24fd9b9ef24a436c751e
CoderOphilia <159889719+CoderOphilia@users.noreply.github.com>
2024-09-18 12:12:13 -0700

    Add files via upload

---
+++ week3Project/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module week3Project {
+}
---
+++ week3Project/src/week3Project/EmployeeDemo.java
@@ -0,0 +1,17 @@
+package week3Project;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		// className objectName = new Constructor();
+        Employee emp1 = new Employee();
+        //System.out.println(emp1.getName()+ " "+ emp1.getEmpId() + " " + emp1.getSalary());
+	    System.out.println(emp1); //only pass the object when there is a toString() function
+	    
+        int id = 4555;
+        Employee emp2 = new Employee(id, "David", 89000);
+        //System.out.println(emp2.getName()+ " "+ emp2.getEmpId() + " " + emp2.getSalary());
+        System.out.println(emp2);
+	}
+
+}
